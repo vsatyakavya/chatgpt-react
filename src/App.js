54d0b9c024f335422a5b7f732a0d8002ae794f51
Satya Kavya Varri <satyakavya24@gmail.com>
2025-02-21 17:49:56 -0600

    added server script and FE logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import axios from 'axios'
+import CGLogo from './chatGPT.png';
+import AppLogo from './app-logo.png';
 import './App.css';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) =>{
+     e.preventDefault();
+     setLoading(true);
+
+     //communicate with openAI API
+     axios
+      .post("http://localhost:5555/chat", {prompt})
+      .then((res) => {
+        setResponse(res.data);
+        setLoading(false);
+      })
+     .catch((err) =>{
+       console.log(err);
+     })
+
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className ="wrapper">
+      <img src ={AppLogo}  alt="" className ="app-logo"/>
+      <form onSubmit={handleSubmit}>
+        <img src={CGLogo} alt="" className ={loading ? "cg-logo loading" : "cg-logo"}/>
+        <input 
+        type = "text"
+        value={prompt}
+        onChange = {(e) => setPrompt(e.target.value)}
+        placeholder = "Ask Anything...)"
+        />
+        <button type="submit">Ask</button>
+      </form>
+
+      <p className='response-area'>
+        {loading ? 'loading...'  : response}
+      </p>
+     </div>
   );
 }
 
